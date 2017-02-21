d80aa10ac9d4a628b40e25af9795434a28698507
nvodoor <nvodoor@gmail.com>
2017-02-20 21:58:32 -0800

    Passed All Tests Up To PlayerView.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,25 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function () {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+    this.on('ended', this.shift);
+    this.on('ended', function () {
+      if (this.length > 0) {
+        this.playFirst();
+      } else if (this.length === 0) {
+        // do nothing
+      }
+    }, this);
+    this.on('dequeue', this.shift);
+  },
+
+  playFirst: function () {
+    this.at(0).play();
   }
+  
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,19 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.collection.on('remove', this.render, this);
+    this.collection.on('add', this.render, this);
   },
 
   render: function() {
+    this.$el.empty();
+    this.collection.forEach(this.renderEntry, this);
     return this.$el;
+  },
+
+  renderEntry: function(entry) {
+    var entryView = new SongQueueEntryView({model: entry});
+    this.$el.append(SongQueueEntryView.prototype.render());
   }
 
 });
