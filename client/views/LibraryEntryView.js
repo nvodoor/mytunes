5b849924689ff5b984f1ed7a8abc0278fe47cb82
nvodoor <nvodoor@gmail.com>
2017-02-21 19:27:49 -0800

    Added a Play Count.

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -3,12 +3,14 @@ var LibraryEntryView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td><%= num %></td>'),
 
   events: {
     'click': function() {
       this.model.play();
       this.model.enqueue();
+      this.model.set('num', this.model.get('num') + 1);
+      this.render();
     },
 
   },
